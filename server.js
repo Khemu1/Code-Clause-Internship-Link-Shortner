fb49116a1a042ea1b1bf8783161722f2e7933915
Khemu <alghost2004@gmail.com>
2024-06-04 02:03:52 +0300

    working on form validation

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { connectDB } from "./db.js";
+import { connectDB, UrlAnatics, Url, User } from "./db.js";
 import { fileURLToPath } from "url";
 import path from "path";
 import { dirname } from "path";
-import bodyParser from "body-parser";
 import multer from "multer";
+import mongoose from "mongoose";
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,12 +16,87 @@ app.use(express.static(path.join(__dirname, "public")));
 connectDB();
 
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 const upload = multer();
 
-app.post("/user", upload.none(), (req, res) => {
-  console.log(req.body);
-  return res.status(200).json({ message: "Registration successful" });
+app.post("/user", upload.none(), async (req, res) => {
+  if (!req.body)
+    return res.status(400).send({
+      success: false,
+      body: "You must provide a body to this request",
+    });
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({
+      email,
+      username,
+      password,
+    });
+    await user.save();
+    return res.status(201).send({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      body: "Something went wrong while inserting",
+    });
+  }
+});
+app.post("/get", async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    console.log(username, email);
+
+    const usersByUsername = await User.find({ username });
+    const usersByEmail = await User.find({ email });
+
+    if (usersByUsername.length === 0 && usersByEmail.length === 0) {
+      return res.status(200).send({
+        success: true,
+        body: "No users found",
+      });
+    }
+
+    if (usersByEmail.length != 0) {
+      if (usersByUsername.length != 0) {
+        return res.status(200).send({
+          success: false,
+          body: {
+            username: "already used username",
+            email: "Already used username",
+          },
+        });
+      }
+      return res.status(200).send({
+        success: false,
+        body: {
+          email: "Already used email",
+        },
+      });
+    }
+
+    if (usersByUsername.length != 0) {
+      if (usersByEmail.length != 0) {
+        return res.status(200).send({
+          success: false,
+          body: {
+            username: "Already used username",
+            email: "Already used username",
+          },
+        });
+      }
+      return res.status(200).send({
+        success: false,
+        body: {
+          username: "Already used username",
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
--- src/Js/form.js
+++ src/Js/form.js
@@ -8,23 +8,24 @@ registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(registerForm);
   let errors;
-  try {
-    await validateUser.validate(
-      {
-        email: formData.get("email"),
-        username: formData.get("username"),
-        password: formData.get("password"),
-      },
-      { abortEarly: false }
-    );
-  } catch (e) {
-    errors = transformYupErrorsIntoObject(e);
-  }
-  if (errors) {
-    console.log("i do have errors", errors);
-    return;
-  }
-  submit(formData);
+  // try {
+  //   await validateUser.validate(
+  //     {
+  //       email: formData.get("email"),
+  //       username: formData.get("username"),
+  //       password: formData.get("password"),
+  //     },
+  //     { abortEarly: false }
+  //   );
+  // } catch (e) {
+  //   errors = transformYupErrorsIntoObject(e);
+  // }
+  // if (errors) {
+  //   console.log("i do have errors", errors);
+  //   return;
+  // }
+  retrieve("ALIFSAFASF");
+  // submit(formData);
   console.log(formData);
 });
 
@@ -37,12 +38,38 @@ function submit(data) {
       if (!response.ok) {
         throw new Error("HTTP error, status = " + response.status);
       }
-      return response.text();
+      return response.json();
     })
     .then((data) => {
       console.log("Data sent: " + data);
     })
     .catch((error) => {
       console.error("Something went wrong when sending data:", error);
+    });
+}
+
+function retrieve(name) {
+  fetch(urlPrefix + "/get", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "ALIFSAFASF",
+      email: "alghost2004@gmail.com",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+      }
+      return response.json(); // Parse the JSON response
+    })
+    .then((data) => {
+      // Access the response body data here
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
